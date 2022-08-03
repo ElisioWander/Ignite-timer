27364d65396f20415026446cdb6a19c3212f7f93
elisioWander <elisio741@hotmail.com>
2022-08-03 18:53:51 -0300

    removing getItem from localStorage

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -2,9 +2,12 @@ import { Timer, Scroll } from 'phosphor-react'
 import { NavLink } from 'react-router-dom'
 import { HeaderContainer } from './styles'
 
+import logoIgnite from '../../assets/logo-ignite.svg'
+
 export function Header() {
   return (
     <HeaderContainer>
+      <img src={logoIgnite} alt="" />
       <nav>
         <NavLink to="/" title="Timer">
           <Timer size={24} />
