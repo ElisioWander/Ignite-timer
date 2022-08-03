1731d14b271b3bc710c40befa6ac6cb2342c1066
elisioWander <elisio741@hotmail.com>
2022-08-03 18:43:24 -0300

    assets removed

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -7,7 +7,6 @@ import logoIgnite from '../../assets/logo-ignite.svg'
 export function Header() {
   return (
     <HeaderContainer>
-      <img src={logoIgnite} alt="" />
       <nav>
         <NavLink to="/" title="Timer">
           <Timer size={24} />
