48f382f30a36b3eb05133934e0312de0247f7752
elisioWander <elisio741@hotmail.com>
2022-08-03 18:48:05 -0300

    assets removed

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -2,8 +2,6 @@ import { Timer, Scroll } from 'phosphor-react'
 import { NavLink } from 'react-router-dom'
 import { HeaderContainer } from './styles'
 
-import logoIgnite from '../../assets/logo-ignite.svg'
-
 export function Header() {
   return (
     <HeaderContainer>
